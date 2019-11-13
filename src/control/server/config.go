c920e5adcd385faa04bed7e92d86f577b1e2d1ee
Li Wei <wei.g.li@intel.com>
2019-11-13 17:03:12 +0800

    DAOS-2822 iosrv: Remove PMIx bootstrap mode
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- src/control/server/config.go
+++ src/control/server/config.go
@@ -74,7 +74,6 @@ type Configuration struct {
 	SocketDir  string                `yaml:"socket_dir"`
 	Fabric     ioserver.FabricConfig `yaml:",inline"`
 	Modules    string
-	Attach     string
 
 	AccessPoints []string `yaml:"access_points"`
 
@@ -159,16 +158,6 @@ func (c *Configuration) WithModules(mList string) *Configuration {
 	return c
 }
 
-// WithAttach sets attachment info path.
-func (c *Configuration) WithAttachInfo(aip string) *Configuration {
-	c.Attach = aip
-	// TODO: Should all instances share this? Thinking probably not...
-	for _, srv := range c.Servers {
-		srv.WithAttachInfoPath(aip)
-	}
-	return c
-}
-
 // WithFabricProvider sets the top-level fabric provider.
 func (c *Configuration) WithFabricProvider(provider string) *Configuration {
 	c.Fabric.Provider = provider
@@ -188,7 +177,6 @@ func (c *Configuration) updateServerConfig(srvCfg *ioserver.Config) {
 	srvCfg.WithShmID(c.NvmeShmID)
 	srvCfg.SocketDir = c.SocketDir
 	srvCfg.Modules = c.Modules
-	srvCfg.AttachInfoPath = c.Attach // TODO: Is this correct?
 }
 
 // WithServers sets the list of IOServer configurations.
